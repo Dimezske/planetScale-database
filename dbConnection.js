1cbe3927613a8f41331d3970d6ae49620239c21a
AJ <dimezske@gmail.com>
2022-03-11 15:36:27 +1100

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 require('dotenv').config()
 const conn = mysql.createConnection(
-  'mysql://i8c7jm8pbmvw:pscale_pw_kw2GcIGNRFRxQK-yPM_BFMjeMEHAMf7ivusxvmoimAc@9plz9uw8seak.us-east-2.psdb.cloud/auth?ssl={"rejectUnauthorized":true}'
+  'mysql://'
 );
 
 conn.query("select * from users;", function (err, _rows, _fields) {
